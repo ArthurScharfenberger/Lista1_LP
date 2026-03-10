70ae92e2214a53eddb118ea5b7f1597a6f3721e1
Arthur Scharfenberger <ascharfen.avilas@hotmail.com>
2026-03-10 13:03:37 -0300

    Lista 1 - Lógica de Programação

---
+++ exemplo10.py
@@ -0,0 +1,9 @@
+x1 = float(input("Informe o valor de x1: "))
+y1 = float(input("Informe o valor de y1: "))
+x2 = float(input("Informe o valor de x2: "))
+y2 = float(input("Informe o valor de y2: "))
+P = x1 , y1
+Q = x2 , y2
+
+D = (x2 - x1)** 2 + (y2 -y1)** 2
+print (f"A distância entre eles é: {D}")
---
+++ exemplo9.py
@@ -0,0 +1,6 @@
+A = float(input("Informe o valor A:"))
+B = float(input("Informe o valor B:"))
+C = float(input("Informe o valor C:"))
+
+soma = (A + B + C)** 2
+print (f"A soma dos quadrados é de: {soma}")
